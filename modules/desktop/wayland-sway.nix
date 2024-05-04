c720adcc33c354b1eb8328c61c235a2d6536fc1f
Grauly <nils.dellemann@web.de>
2024-05-04 21:19:04 +0200

    feat: more swaying

--- modules/desktop/wayland-sway.nix
+++ modules/desktop/wayland-sway.nix
@@ -1,6 +1,29 @@
+{ pkgs, ... }:
 {
   programs.sway = {
     enable = true;
     wrapperFeatures.gtk = true;
+  };
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+  };
+
+  security.polkit.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+  security.pam.services.swaylock = { };
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.greetd}/bin/agreety --cmd sway";
+      };
+    };
   };
 }
