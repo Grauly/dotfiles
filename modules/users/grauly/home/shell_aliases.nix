2d6ea5804938f5b145fdcb41fc89d62dbfb47a04
Grauly <nils.dellemann@web.de>
2024-05-05 14:05:06 +0200

    feat: more shell aliases

--- modules/users/grauly/home/shell_aliases.nix
+++ modules/users/grauly/home/shell_aliases.nix
@@ -1,5 +1,10 @@
 {
   home.shellAliases = {
     ll = "ls - lah";
+    lsstick = "lsblk | grep -v  \"nvme\"";
+    mntstick = "udisksctl mount -b /dev/";
+    umntstick = "udisksctl unmount -b /dev/";
+    reswitch = "sudo nixos-rebuild switch --flake /home/grauly/dotfiles#thetanix";
+    reswitch-boot = "sudo nixos-rebuild boot --flake /home/grauly/dotfiles#thetanix";
   };
 }
