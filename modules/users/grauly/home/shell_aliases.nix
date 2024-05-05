c5f9b9de10ee0448187d1350ac515da2e18bd379
Grauly <nils.dellemann@web.de>
2024-05-05 16:10:51 +0200

    feat: more shell aliases

--- modules/users/grauly/home/shell_aliases.nix
+++ modules/users/grauly/home/shell_aliases.nix
@@ -4,7 +4,9 @@
     lsstick = "lsblk | grep -v  \"nvme\"";
     mntstick = "() {udisksctl mount -b \"/dev/\$1\"}";
     umntstick = "() {udisksctl unmount -b \"/dev/\$1\"}";
+    lsmntstick = "ls /run/media/grauly/";
     reswitch = "sudo nixos-rebuild switch --flake /home/grauly/dotfiles#thetanix";
     reswitch-boot = "sudo nixos-rebuild boot --flake /home/grauly/dotfiles#thetanix";
+    rsrc = "source ~/.zshrc";
   };
 }
