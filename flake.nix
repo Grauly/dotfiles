9230a3e45406799826146ad7d658050ef5d8010e
Grauly <nils.dellemann@web.de>
2024-05-05 02:09:22 +0200

    feat: nvidia drivers

--- flake.nix
+++ flake.nix
@@ -39,7 +39,7 @@
           inherit system pkgs;
           modules = [
             ./modules/modules.nix
-            ./hardware-configuration.nix
+            ./hardware/hardware.nix
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
