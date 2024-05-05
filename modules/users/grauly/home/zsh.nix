f4373e9e9979e1a73a563d3e5fdb78862098b302
Grauly <nils.dellemann@web.de>
2024-05-05 13:18:15 +0200

    feat: set up autosuggestions

--- modules/users/grauly/home/zsh.nix
+++ modules/users/grauly/home/zsh.nix
@@ -4,9 +4,11 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    enableAutosuggestions = true;
     autocd = true;
     syntaxHighlighting.enable = true;
     history.size = 10000;
     history.path = "${config.xdg.dataHome}/zsh/history";
+    oh-my-zsh.enable = true;
   };
 }
