8f15b187308e4eed3f68e01c9925c9d574b5bd31
Grauly <nils.dellemann@web.de>
2024-05-08 20:29:44 +0200

    feat: first steps of wofi styling

--- modules/users/grauly/home/programs/wofi.nix
+++ modules/users/grauly/home/programs/wofi.nix
@@ -1,5 +1,28 @@
 {
   programs.wofi = {
     enable = true;
+    settings = {
+      insensitive = true;
+    };
+    style = ''
+      * {
+        font-family: "JetBrains Mono";
+        background-color: transparent;
+        color: white;
+      }
+      window {
+        background-color: rgba(32,37,37,0.7);
+        border-radius: 10px;
+        border: 2px solid #430b9e;
+      }
+      #entry:selected {
+        background-color: #430b9e;
+        color: white;
+        border: none;
+      }
+      scrollbar {
+        backgrouncolor: #2642bf;
+      }
+    '';
   };
 }
