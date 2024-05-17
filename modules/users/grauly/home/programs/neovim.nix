1f0564226978406b140a332ab183895f6c80038f
Grauly <nils.dellemann@web.de>
2024-05-18 01:55:41 +0200

    feat: nvim should now copy to clipboard

--- modules/users/grauly/home/programs/neovim.nix
+++ modules/users/grauly/home/programs/neovim.nix
@@ -39,7 +39,7 @@
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-
+      vim.opt.clipboard = 'unnamedplus'
     '';
   };
 }
