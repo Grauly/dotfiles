d6e67e0b392c763184bc12af7909cb7714cdc94a
Grauly <nils.dellemann@web.de>
2024-05-19 03:16:25 +0200

    feat: shell alias to make test builds easier

--- modules/users/grauly/home/shell_aliases.nix
+++ modules/users/grauly/home/shell_aliases.nix
@@ -8,6 +8,7 @@
     lsmntstick = "ls /run/media/grauly/";
     reswitch = "sudo nixos-rebuild switch --flake /home/grauly/dotfiles#thetanix";
     reswitch-boot = "sudo nixos-rebuild boot --flake /home/grauly/dotfiles#thetanix";
+    retest = "sudo nixos-rebuild text --flake /home/grauly/dotfiles#thetanix";
     rsrc = "source ~/.zshrc";
     rebar = "systemctl --user restart waybar";
     reswitch-bar = "reswitch; rebar";
