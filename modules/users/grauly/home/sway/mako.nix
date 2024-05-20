d62d875ed3f1b197ba10a96ffc9bd3e86d5cc504
Grauly <nils.dellemann@web.de>
2024-05-20 18:09:03 +0200

    feat: mako styling

--- modules/users/grauly/home/sway/mako.nix
+++ modules/users/grauly/home/sway/mako.nix
@@ -1,7 +1,19 @@
+{ osConfig, ... }:
+
 {
-  services.mako = {
+  services.mako = with osConfig.grauly; {
     enable = true;
     defaultTimeout = 10 * 1000;
     anchor = "bottom-right";
+    backgroundColor = colors.background;
+    borderColor = colors.mix;
+    borderRadius = style.border-radius;
+    borderSize = style.border-width;
+    progressColor = "over ${colors.highlight}"; 
+    textColor = colors.text.color;
+    extraConfig = ''
+      [urgency=critical]
+      border-color=${colors.highlight}
+    '';
   };
 }
