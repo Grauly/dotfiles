cf1f13ee50c6e0851c6210952963f77cf48283ad
Grauly <nils.dellemann@web.de>
2024-06-05 14:38:59 +0200

    fix: removed not working adwatio stuff

--- modules/gtk.nix
+++ modules/gtk.nix
@@ -1,6 +1,8 @@
 {
   environment.etc = {
-    "xdg/gtk-2.0/gtkrc".text = "gtk-application-prefer-dark-theme=1";
+    "xdg/gtk-2.0/gtkrc".text = ''
+    gtk-application-prefer-dark-theme=1
+    '';
     "xdg/gtk-3.0/settings.ini".text = ''
       [Settings]
       gtk-application-prefer-dark-theme=1
