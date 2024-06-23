4757b51b6f11f5d39ce89c7342d483edd4057499
Grauly <nils.dellemann@web.de>
2024-06-23 02:05:02 +0200

    feat: added zsh flavor

--- modules/users/grauly/home/programs/zsh.nix
+++ modules/users/grauly/home/programs/zsh.nix
@@ -1,14 +1,33 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 
 {
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = false;
+  };
+
+  programs.oh-my-posh = {
+    enable = true;
+    useTheme = "microverse-power";
+  };
+
+  home.packages = with pkgs; [
+  ];
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
-    autocd = true;
     syntaxHighlighting.enable = true;
     history.size = 10000;
     history.path = "${config.xdg.dataHome}/zsh/history";
-    oh-my-zsh.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "colored-man-pages"
+        "zoxide"
+      ];
+    };
   };
 }
