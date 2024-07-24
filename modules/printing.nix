b0790a5d5f160ab47caf668bdcaf3c84661632f6
Grauly <nils.dellemann@web.de>
2024-07-25 00:37:34 +0200

    feat: more strictly specif printer drivers

--- modules/printing.nix
+++ modules/printing.nix
@@ -7,7 +7,7 @@
   };
   hardware.printers.ensurePrinters = [{
     name = "Space-2d-Drucker";
-    model = "HP/hp-color_laserjet_mfp_m480-ps.ppd.gz";
+    model = "HP/hp-color_laserjet_pro_mfp_m477-ps.ppd.gz";
     deviceUri = "socket://2d.sfz-aalen.space";
   }];
 }
