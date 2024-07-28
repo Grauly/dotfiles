b5a82ba3d622867aa24f629631b8e154c3dc328d
Grauly <nils.dellemann@web.de>
2024-07-28 04:51:29 +0200

    chore: attempts to get nautilus to parse videos

--- modules/nautilus.nix
+++ modules/nautilus.nix
@@ -1,6 +1,19 @@
 { pkgs, ... }:
 
 {
+  nixpkgs.overlays = [
+    (self: super: {
+      gnome = super.gnome.overrideScope' (gself: gsuper: {
+        nautilus = gsuper.nautilus.overrideAttrs (nsuper: {
+          buildInputs = nsuper.buildInputs ++ (with pkgs.gst_all_1; [
+            gst-plugins-good
+            gst-plugins-bad
+          ]);
+        });
+      });
+    })
+  ];
+
   environment.systemPackages = with pkgs; [
     gnome.nautilus
   ];
