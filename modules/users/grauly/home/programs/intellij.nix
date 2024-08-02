86feee03792f1e32a562a2810884ac20c5316980
Grauly <nils.dellemann@web.de>
2024-08-03 01:25:35 +0200

    revert: no more global path setting
    This might revive projectOR
    This reverts commit fbc1ad77d2da5776ca9fd26402716e20aa6d2824.

--- modules/users/grauly/home/programs/intellij.nix
+++ modules/users/grauly/home/programs/intellij.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
   home.packages = with pkgs; [
@@ -8,14 +8,6 @@
   home.file = {
     ".jdks/openjdk17".source = "${pkgs.jdk17}/lib/openjdk";
     ".jdks/openjdk21".source = "${pkgs.jdk21}/lib/openjdk";
-  };
-
-  # to enable actually starting MC from dev env, taken from  https://discourse.nixos.org/t/issues-with-opengl-in-minecraft-development-environments/37532/10
-  home.sessionVariables = {
-    LD_LIBRARY_PATH = lib.makeLibraryPath [
-      pkgs.libglvnd
-      pkgs.pulseaudio
-    ];
   };
 }
 
