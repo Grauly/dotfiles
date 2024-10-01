c41b772c59776262babb4110d03889d49cbca614
Grauly <nils.dellemann@web.de>
2024-10-01 19:56:53 +0200

    fail: trying to add printer

--- modules/printing.nix
+++ modules/printing.nix
@@ -3,11 +3,24 @@
 {
   services.printing = {
     enable = true;
-    drivers = [ pkgs.hplip ];
+    drivers = with pkgs; [
+      hplip
+      brlaser
+      brgenml1lpr
+      brgenml1cupswrapper
+      dcp9020cdw-cupswrapper
+    ];
   };
-  hardware.printers.ensurePrinters = [{
-    name = "Space-2d-Drucker";
-    model = "HP/hp-color_laserjet_pro_mfp_m477-ps.ppd.gz";
-    deviceUri = "socket://2d.sfz-aalen.space";
-  }];
+  hardware.printers.ensurePrinters = [
+    {
+      name = "Space-2d-Drucker";
+      model = "HP/hp-color_laserjet_pro_mfp_m477-ps.ppd.gz";
+      deviceUri = "socket://2d.sfz-aalen.space";
+    }
+    {
+      name = "Home-2d-Drucker";
+      model = "brother_dcp9020cdw_printer_en.ppd";
+      deviceUri = "socket://192.168.11.29";
+    }
+  ];
 }
