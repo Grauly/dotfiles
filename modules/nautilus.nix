1a28a3790f992ecec8279b421922e321954e6ad3
Grauly <nils.dellemann@web.de>
2024-12-01 01:00:23 +0100

    fix: fixup nautilus being moved

--- modules/nautilus.nix
+++ modules/nautilus.nix
@@ -3,19 +3,17 @@
 {
   nixpkgs.overlays = [
     (self: super: {
-      gnome = super.gnome.overrideScope' (gself: gsuper: {
-        nautilus = gsuper.nautilus.overrideAttrs (nsuper: {
+        nautilus = super.nautilus.overrideAttrs (nsuper: {
           buildInputs = nsuper.buildInputs ++ (with pkgs.gst_all_1; [
             gst-plugins-good
             gst-plugins-bad
           ]);
         });
-      });
     })
   ];
 
   environment.systemPackages = with pkgs; [
-    gnome.nautilus
+    nautilus
   ];
 
   programs.nautilus-open-any-terminal = {
