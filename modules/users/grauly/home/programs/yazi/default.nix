9595b572f6e5bafd047d04a1ed0a1975847158d5
Grauly <nils.dellemann@web.de>
2025-03-30 00:58:35 +0100

    feat: yazi opener configs

--- modules/users/grauly/home/programs/yazi/default.nix
+++ modules/users/grauly/home/programs/yazi/default.nix
@@ -6,5 +6,6 @@
     #TODO: 1) zipping/unzipping
     #      2) open with
     #      3) drag and drop capability simulation
+    #      4) copy paste support from clipboard
   ];
 }
--- modules/users/grauly/home/programs/yazi/yazi-config.nix
+++ modules/users/grauly/home/programs/yazi/yazi-config.nix
@@ -1,10 +1,61 @@
 { ... }:
 
+let 
+  simpleProgams = [
+    "nvim"
+    "blender"
+    "inkscape"
+    "ark"
+    "gimp"
+    "evince"
+    "blockbench"
+    "prusa-slicer"
+  ];
+in 
 {
   manager = {
     sort_by = "mtime";
     sort_reverse = true;
     sort_dir_first = true;
     linemode = "custom";
+  };
+  opener = ({
+    vlc = [
+      {
+        run = "vlc \"$@\"";
+        orphan = true;
+      }
+    ];
+    librewolf = [
+      {
+        run = "librewolf --new-window \"$@\"";
+        orphan = true;
+      }
+    ];
+  } // (
+    builtins.listToAttrs (map (command:
+      ({
+        name = "${command}";
+        value = [{run = "${command} \"$@\""; block = true;}];
+      })
+    ) simpleProgams)
+  ));
+  open = {
+    rules = [
+      {mime = "text/html"; use = ["nvim" "librewolf"];}
+      {name = "*.gcode"; use = "prusa-slicer";}
+      {name = "*.bgcode"; use = "prusa-slicer";}
+      {mime = "text/*"; use = "nvim";}
+      {mime = "video/*"; use = "vlc";}
+      {mime = "audio/*"; use = "vlc";}      
+      {mime = "image/svg+xml"; use = ["inkscape" "nvim"];}
+      {mime = "image/*"; use = "gimp";}
+      {name = "*.bbmodel"; use = "blockbench";}
+      {name = "*.json"; use = ["nvim" "blockbench" "librewolf"];}
+      {name = "*.zip"; use = "ark";}
+      {name = "*.stl"; use = "prusa-slicer";}
+      {name = "*.pdf"; use = ["evince" "librewolf"];}
+      {mime = "application/x-blender"; use = "blender";}
+    ];
   };
 }
