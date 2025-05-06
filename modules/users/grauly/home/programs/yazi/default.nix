78e059f12a890175e235d9b782a880ca5cc38723
Grauly <nils.dellemann@web.de>
2025-05-07 00:36:07 +0200

    docs: update todo list

--- modules/users/grauly/home/programs/yazi/default.nix
+++ modules/users/grauly/home/programs/yazi/default.nix
@@ -6,9 +6,12 @@
     #./yazi-init.nix
     #TODO: 1) zipping/unzipping
     #      2) open with
-    #      3) drag and drop capability simulation
     #      4) copy paste support from clipboard
     #      5) bookmarks (aka, stationary zoxide)
     #      6) cryfs support
+    #      7) Screenshot integraton (probably with clipboard integ)
+    #      8) Send to Device (KDE Connect)
+    #DONE:
+    #      3) drag and drop capability simulation
   ];
 }
