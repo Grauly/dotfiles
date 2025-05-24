2ecf7cb761c4429c460865050bde2da5520afe6d
Grauly <nils.dellemann@web.de>
2025-05-24 16:06:44 +0200

    fix: fix vscode config for 25.05

--- modules/users/grauly/home/programs/vscode/vscode.nix
+++ modules/users/grauly/home/programs/vscode/vscode.nix
@@ -3,13 +3,15 @@
 {
   programs.vscode = {
     enable = true;
-    enableUpdateCheck = false;
-    enableExtensionUpdateCheck = false;
     mutableExtensionsDir = false;
-    userSettings = (import ./settings.json.nix);
-    extensions = with pkgs.vscode-extensions; [
-      asvetliakov.vscode-neovim
-      sumneko.lua
-    ];
+    profiles.default = {
+      enableUpdateCheck = false;
+      enableExtensionUpdateCheck = false;
+      userSettings = (import ./settings.json.nix);
+      extensions = with pkgs.vscode-extensions; [
+        asvetliakov.vscode-neovim
+        sumneko.lua
+      ];
+    };
   };
 }
