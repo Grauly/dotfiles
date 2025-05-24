50c60da63d783e2a10b8980e89c25801a0302530
Grauly <nils.dellemann@web.de>
2025-05-24 16:07:41 +0200

    feat: fix mako for 25.05

--- modules/users/grauly/home/sway/mako.nix
+++ modules/users/grauly/home/sway/mako.nix
@@ -3,17 +3,18 @@
 {
   services.mako = with osConfig.grauly; {
     enable = true;
-    defaultTimeout = 10 * 1000;
-    anchor = "bottom-right";
-    backgroundColor = colors.background;
-    borderColor = colors.mix;
-    borderRadius = style.border-radius;
-    borderSize = style.border-width;
-    progressColor = "over ${colors.highlight}";
-    textColor = colors.text.color;
-    extraConfig = ''
-      [urgency=critical]
-      border-color=${colors.highlight}
-    '';
+    settings = {
+      defaultTimeout = 10 * 1000;
+      anchor = "bottom-right";
+      backgroundColor = colors.background;
+      borderColor = colors.mix;
+      borderRadius = style.border-radius;
+      borderSize = style.border-width;
+      progressColor = "over ${colors.highlight}";
+      textColor = colors.text.color;
+      "urgency=critical" = {
+        border-color="${colors.highlight}";
+      };
+    };
   };
 }
