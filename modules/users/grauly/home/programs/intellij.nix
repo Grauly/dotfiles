4450de124d8bcb8c5d1bd1167a922637dd942402
Grauly <nils.dellemann@web.de>
2025-07-20 00:21:57 +0200

    feat: make IntelliJ run with needed MCdev deps

--- modules/users/grauly/home/programs/intellij.nix
+++ modules/users/grauly/home/programs/intellij.nix
@@ -1,8 +1,26 @@
 { pkgs, ... }:
 
+let 
+  mc-depencencies = with pkgs; [
+    libpulseaudio
+    libGL
+    glfw
+    openal
+    stdenv.cc.cc.lib  
+  ];
+in 
 {
   home.packages = with pkgs; [
-    jetbrains.idea-community-bin
+    (pkgs.symlinkJoin {
+      name = "IntelliJ Minecraft Support";
+      paths = [ pkgs.jetbrains.idea-community];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/idea-community \
+          --add-flag -Dawt.toolkit.name=WLToolkit \
+          --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath mc-depencencies}
+      '';
+    })
   ];
 
   home.file = {
