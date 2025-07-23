8abf5c5b3386e4c8b8fae9c3040cad8f339994c1
Grauly <nils.dellemann@web.de>
2025-07-23 02:12:28 +0200

    feat: less foreign code in my repo.

--- modules/users/grauly/home/programs/glfw-minecraft/default.nix
+++ modules/users/grauly/home/programs/glfw-minecraft/default.nix
@@ -6,8 +6,13 @@
     url = "https://github.com/llyyr/glfw-minecraft-wayland";
     hash = "sha256-gO/X/Rpf50c3GnX2pUNdeBiT9ugWtQVJnj9YHufkFd8=";
   };
-  patches = pkgs.glfw3.patches
-  ++ [
-    ./0009-Defer-setting-cursor-position-until-the-cursor-is-lo.patch
-  ];
+  patches = [
+    # yes, I basically patch the thing in the other patch patches out
+    # I just could not find a way to patch the patch without having a duplicate of the entire nixpkgs flying around
+    # (and even then I could not figure out how to get the package out of there)
+    # so, this is the best for now.
+    # the patch in question:
+    # https://github.com/NixOS/nixpkgs/blob/nixos-25.05/pkgs/by-name/gl/glfw3/0009-Defer-setting-cursor-position-until-the-cursor-is-lo.patch
+    ./patch-in-for-patch-out.patch
+  ] ++ previousAttrs.patches;
 }))
