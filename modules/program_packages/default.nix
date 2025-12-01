61f1277f3d30826cae01061d4251fb7c5aaab468
Grauly <nils.dellemann@web.de>
2025-12-01 21:57:22 +0100

    feat: base desktop env package

--- modules/program_packages/default.nix
+++ modules/program_packages/default.nix
@@ -1,16 +1,6 @@
 {
   imports = [
+    ./desktop_env.nix
     ./core_utils.nix
-    ./communication.nix
-    ./creative.nix
-    ./explorer_utils.nix
-    ./extra_utils.nix
-    ./gaming.nix
-    ./latex.nix
-    ./minecraft_dev.nix
-    ./python_dev.nix
-    ./recording.nix
-    ./work.nix
-    ./godot_dev.nix
   ];
 }
---
+++ modules/program_packages/desktop_env.nix
@@ -0,0 +1,11 @@
+{
+  imports = [
+    ../programs/application_launcher
+    ../programs/browser
+    ../programs/desktop_environment
+    ../programs/file_explorer
+    ../programs/shell
+    ../programs/terminal
+    ../programs/waybar
+  ];
+}
