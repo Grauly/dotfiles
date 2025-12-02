196b91ba285dade54889764dd7ac8ed66f8de5d8
Grauly <nils.dellemann@web.de>
2025-12-02 22:50:51 +0100

    fix: add dconf, since apparently thats needed now?

--- user/gtk/default.nix
+++ user/gtk/default.nix
@@ -5,6 +5,11 @@
   ];
   home-manager.users.grauly = {
     gtk.enable = true;
+    home.packages = with pkgs; [
+      # apparently needed, but why is it only now an issue?
+      dconf
+    ];
+    
     imports = [
       ./cursor
     ];
