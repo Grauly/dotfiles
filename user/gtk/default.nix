0ed5adf913845e73fc23db2b8dfe560e700da5a0
Grauly <nils.dellemann@web.de>
2025-12-02 22:59:55 +0100

    fix: move dcof to sys level

--- user/gtk/default.nix
+++ user/gtk/default.nix
@@ -3,12 +3,11 @@
     ./gtk.nix
     ./gtk_file-picker-fix.nix
   ];
+  
+  programs.dconf.enable = true;
+
   home-manager.users.grauly = {
     gtk.enable = true;
-    home.packages = with pkgs; [
-      # apparently needed, but why is it only now an issue?
-      dconf
-    ];
     
     imports = [
       ./cursor
