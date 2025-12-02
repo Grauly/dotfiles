7c44b25f84f0e37b60a2083ff12b30db66833be0
Grauly <nils.dellemann@web.de>
2025-12-02 18:59:42 +0100

    fix: move things out of home manager, for a change

--- user/gtk/default.nix
+++ user/gtk/default.nix
@@ -1,10 +1,12 @@
 {
+  imports = [
+    ./gtk.nix
+    ./gtk_file-picker-fix.nix
+  ];
   home-manager.users.grauly = {
     gtk.enable = true;
     imports = [
       ./cursor
-      ./gtk_file-picker-fix.nix
-      ./gtk.nix
     ];
   };
 }
