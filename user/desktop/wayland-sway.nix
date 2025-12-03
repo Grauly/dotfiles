f83c97336ba4d4a9354b5438f012735b40f2cea7
Grauly <nils.dellemann@web.de>
2025-12-04 00:37:43 +0100

    chore: rename greetd to stop warning

--- user/desktop/wayland-sway.nix
+++ user/desktop/wayland-sway.nix
@@ -30,7 +30,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.greetd}/bin/agreety --cmd \"sway --unsupported-gpu\"";
+        command = "${pkgs.greetd}/bin/agreety --cmd \"sway --unsupported-gpu\"";
       };
     };
   };
