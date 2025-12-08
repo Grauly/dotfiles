53251b7536abd91e99bdf0c2878a83587b1ac380
Grauly <nils.dellemann@web.de>
2025-12-08 22:05:02 +0100

    feat: initial waybar refactors
    its quite broken rn

--- modules/programs/waybar/waybar_settings.nix
+++ modules/programs/waybar/waybar_settings.nix
@@ -7,13 +7,55 @@ let
   pulsemixer = "${pkgs.pulsemixer}/bin/pulsemixer";
   floating_shell = "exec ${terminal} --detach -T floating_shell ${shell} -c";
   conn_stats_script = "${pkgs.unixtools.ping}/bin/ping 8.8.8.8 -i 1 -c 5 -q | ${pkgs.gnused}/bin/sed -rn 's#([0-9]+) packets transmitted, ([0-9]+) received, ([0-9]+)% packet loss, time [0-9]+ms#{\"text\":\"\\2/\\1 Packets received, \\3% Packet loss\", \"alt\":\"\\2/\\1@\\3%\", \"tooltip\":\"\\2/\\1@\\3% packet loss\", \"class\":\"noclass\",\"percentage\":\\3}#p'";
+  cores = {core}: if core == 0 then "{icon${toString core}}" else "${cores {core = (core - 1);}}{icon${toString core}}";
+  bar-height = 40;
 in
 {
   layer = "top";
   position = "top";
   modules-left = [ "niri/workspaces" ];
   modules-center = [ "clock#minimal" "sway/mode" ];
-  modules-right = [ "custom/arrow_left" "cpu" "memory" "custom/inv_arrow_right" "custom/arrow_left" "custom/connectivity" "network" "custom/arrow_right" "custom/inv_arrow_left" "pulseaudio" "backlight" "battery" "custom/arrow_right" "custom/inv_arrow_left" "clock" ];
+  modules-right = [
+    "image#seperator#left"
+    "group/cpu"
+    "image#seperator#right"
+    "memory"
+    "custom/connectivity"
+    "network"
+    "pulseaudio"
+    "backlight"
+    "battery"
+    "clock"
+    ];
+
+  "image#seperator#left" = {
+    path = "${./assets/seperator-left.svg}";
+    size = bar-height;
+  };
+
+  "image#seperator#right" = {
+    path = "${./assets/seperator-right.svg}";
+    size = bar-height;
+  };
+
+  "image#cpu" = {
+    path = "${./assets/cpu.svg}";
+    size = bar-height - 5;
+  };
+
+  cpu = {
+    interval = 1;
+    format = "${(cores {core = 19; })}";
+    format-icons = [" " "▁" "▂" "▃" "▄" "▅" "▆" "▇" "█"];
+  };
+  
+  "group/cpu" = {
+    orientation = "inherit";
+    modules = [
+      "image#cpu"
+      "cpu"
+    ];
+  };
 
   battery = {
     interval = 5;
@@ -91,15 +133,6 @@ in
     on-click = "${floating_shell} ${nmtui}";
   };
 
-  cpu = {
-    format = "CPU: {usage}% @ {avg_frequency}GHz";
-    states = {
-      ok = 50;
-      in-use = 70;
-      warning = 90;
-      critical = 100;
-    };
-  };
 
   memory = {
     format = "RAM: {used}/{total}GB: {percentage}%";
