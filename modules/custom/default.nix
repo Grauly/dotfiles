eb949d686e4bf4d787e7522dea8bd26cf3713b93
Grauly <nils.dellemann@web.de>
2025-12-13 17:53:35 +0100

    feat: gpu util coloring

--- modules/custom/default.nix
+++ modules/custom/default.nix
@@ -1,6 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   environment.systemPackages = [
     (import ./pkg-trace.nix { inherit pkgs; })
+    (import ../programs/waybar/scripts/gpu_utilization.nix { pkgs = pkgs; osConfig = config; })
   ];
 }
