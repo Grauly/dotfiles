e167bdbcbdbd6cfe45b688e075ed878340e21afc
Grauly <nils.dellemann@web.de>
2025-12-13 18:54:39 +0100

    feat: vram usage

--- modules/custom/default.nix
+++ modules/custom/default.nix
@@ -3,5 +3,6 @@
   environment.systemPackages = [
     (import ./pkg-trace.nix { inherit pkgs; })
     (import ../programs/waybar/scripts/gpu_utilization.nix { pkgs = pkgs; osConfig = config; })
+    (import ../programs/waybar/scripts/gpu_vram_usage.nix { pkgs = pkgs; osConfig = config; })
   ];
 }
