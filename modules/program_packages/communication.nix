480d4a9e0acfa951b913134999c811a2325cee38
Grauly <nils.dellemann@web.de>
2026-02-21 15:17:50 +0100

    feat: add more communication methods

--- modules/program_packages/communication.nix
+++ modules/program_packages/communication.nix
@@ -10,6 +10,9 @@
     thunderbird
     element-desktop
     bitwarden-desktop
+    mumble
+    teamspeak6-client
+    signal-desktop
   ] ++ (with pkgs-unstable; [
     #https://github.com/bitwarden/clients/issues/8759
     vesktop
