435e453491a11e5ed4aebdb852b359773e6cc491
Grauly <nils.dellemann@web.de>
2026-02-28 00:49:05 +0100

    feat: add openjdk25

--- modules/programs/intellij/intellij.nix
+++ modules/programs/intellij/intellij.nix
@@ -4,6 +4,7 @@
   home.file = {
     ".jdks/openjdk17".source = "${pkgs.jdk17}/lib/openjdk";
     ".jdks/openjdk21".source = "${pkgs.jdk21}/lib/openjdk";
+    ".jdks/openjdk25".source = "${pkgs.jdk25}/lib/openjdk";
     ".jdks/jetbrainsjdk".source = "${pkgs.jetbrains.jdk}/lib/openjdk";
     #".jdks/jetbrainsjdk25".source = "${import ./jbr25.nix { pkgs = pkgs; withJcef = false; }}/lib/openjdk";
   };
