ebc01f071eee1b4a2669a33c3c36006ba68dcf6d
Grauly <nils.dellemann@web.de>
2026-04-29 21:42:43 +0200

    feat: more clang config

--- modules/programs/neovim/modules/clang_lsp.nix
+++ modules/programs/neovim/modules/clang_lsp.nix
@@ -7,7 +7,18 @@
   lua-config = ''
     vim.lsp.config['clangd'] = {
       cmd = { '${pkgs.clang-tools}/bin/clangd' },
+      
+      on_attach = function(client, bufnr)
+            vim.lsp.completion.enable(true, client.id, bufnr, {
+              autotrigger = true,
+              convert = function(item)
+                return { abbr = item.label:gsub("%b()", "") }
+              end,
+            })
+            vim.keymap.set("i", "<C-space>", vim.lsp.completion.get, { desc = "trigger autocompletion" })
+          end
     }
     vim.lsp.enable('clangd')
+    vim.opt.completeopt = { "menuone", "noselect", "popup" } 
   '';
 }
