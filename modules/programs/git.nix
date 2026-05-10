9fdfae5c78cf145ca070e8229db5329fe127f586
Grauly <nils.dellemann@web.de>
2026-05-10 14:45:26 +0200

    feat: add commit signing

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -1,19 +1,39 @@
-{ home-manager, pkgs, ... }:
+{
+  home-manager,
+  config,
+  pkgs,
+  ...
+}:
 
 {
   environment.systemPackages = with pkgs; [
     git
   ];
 
-  home-manager.users.grauly.programs.git = {
-    enable = true;
-    settings = {
-      user = {
-        email = "nils.dellemann@web.de";
-        name = "Grauly";
+  home-manager.users.grauly =
+    let
+      mail = "nils.dellemann@web.de";
+      user = "Grauly";
+      signingkey = "${config.home-manager.users.grauly.home.homeDirectory}/.ssh/signingkey.pub";
+      allowedSignersFile = "${config.home-manager.users.grauly.home.homeDirectory}/.ssh/allowed_signers";
+    in
+    {
+      programs.git = {
+        enable = true;
+        settings = {
+          user = {
+            email = mail;
+            name = user;
+          };
+          push.autoSetupRemote = true;
+          init.defaultBranch = "main";
+          gpg.ssh.allowedSignersFile = allowedSignersFile;
+        };
+        signing = {
+          signByDefault = true;
+          format = "ssh";
+          key = signingkey;
+        };
       };
-      push.autoSetupRemote = true;
-      init.defaultBranch = "main";
     };
-  };
 }
