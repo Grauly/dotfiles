c4677a7af0b153ec614c99cb582a28abc3d36345
Grauly <nils.dellemann@web.de>
2026-06-02 18:15:50 +0200

    feat: base update to 26.05

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
 
   inputs = {
     nixpkgs = {
-      url = "nixpkgs/nixos-25.11";
+      url = "nixpkgs/nixos-26.05";
     };
     nixpkgs-unstable = {
       url = "nixpkgs/nixos-unstable";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs-blender = {
